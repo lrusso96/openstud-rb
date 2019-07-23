19ac0c4fc3cdc05dd401aa09dc56fd279d7d2c3a
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-23 17:05:27 +0200

    BaseError stub

---
+++ lib/openstud/error.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Openstud
+  module Exceptions
+    class BaseError < StandardError
+      attr_accessor :types
+
+      def initialize(msg, types: [])
+        super(msg)
+        @types = types
+        i = 0
+        types.each do |s|
+          s = s.to_s
+          self.class.send(:define_method,s + '!') do
+            @types[i] = true
+            self
+          end
+          self.class.send(:define_method,s + '?') { @types[i] }
+          i += 1
+        end
+      end
+    end
+
+    require_relative './exceptions/base_login_error.rb'
+    require_relative './exceptions/refresh_error.rb'
+  end
+end
