feb93528fb7a9fd594813dcf4681ad16cd4c579f
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-23 19:08:56 +0200

    tests for successful and unsuccessful login

--- spec/openstud_spec.rb
+++ spec/openstud_spec.rb
@@ -2,14 +2,19 @@
 
 RSpec.describe Openstud do
   it 'has a version number' do
-    expect(Openstud::VERSION).not_to be nil
+    expect(Openstud::VERSION).to be '0.0.1'
   end
 
-  it 'does something' do
+  it 'succesfully logins' do
     base = Openstud::Base.new
-    base.studentID = '1234567'
-    base.password = 'password'
-    # base.login
-    # expect(base.token.size).to be > 0
+    base.student_id = ENV['STUDENT_ID']
+    base.password = ENV['STUDENT_PWD']
+    base.login
+    expect(base.token.empty?).to be false
+  end
+
+  it 'raises RefreshException for invalid credentials' do
+    base = Openstud::Base.new student_id: 1234567, password: 'password'
+    expect { base.login }.to raise_error(Openstud::Exceptions::RefreshError)
   end
 end
