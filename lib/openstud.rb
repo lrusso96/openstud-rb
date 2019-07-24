c6e10a5d71a1cabc73777ef743d3124ce2513ed1
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-24 16:59:54 +0200

    phoenix module

--- lib/openstud.rb
+++ lib/openstud.rb
@@ -3,8 +3,9 @@
 require 'openstud/version'
 
 module Openstud
-  ENDPOINT_API = 'https://www.studenti.uniroma1.it/phoenixws'
+  require_relative './openstud/phoenix.rb'
   require_relative './openstud/error.rb'
   require_relative './openstud/core/services/authenticator.rb'
+  require_relative './openstud/core/services/bio.rb'
   require_relative './openstud/base.rb'
 end
---
+++ lib/openstud/phoenix.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'httparty'
+
+module Openstud
+  ENDPOINT_API = 'https://www.studenti.uniroma1.it/phoenixws'
+
+  class Phoenix
+    include HTTParty
+
+    attr_reader :headers
+    attr_reader :options
+
+    def initialize(body: {})
+      @headers = { 'Content-EventType' => 'application/x-www-form-urlencoded',
+                   Accept: 'application/json' }
+      @options = { headers: @headers, body: body}
+    end
+
+    def login
+      easy self.class.post("#{ENDPOINT_API}/autenticazione", @options)
+    end
+
+    def info_student(id, token)
+      easy self.class.get("#{ENDPOINT_API}/studente/#{id}?ingresso=#{token}", @options)
+    end
+
+    private
+
+    def easy(res)
+      JSON.parse res.body
+    end
+  end
+end
