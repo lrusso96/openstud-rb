5d19e87829b4bb43e86d725d84521667548baa63
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-24 17:00:43 +0200

    use phoenix in authenticator

--- lib/openstud/core/services/authenticator.rb
+++ lib/openstud/core/services/authenticator.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'httparty'
 
 module Openstud
+  # Namespace for methods that handle authentication, e.g. login
+  #
+  # @since 0.1.0
   module Authenticator
     include Exceptions
 
-    def login
+    # Authenticates the current [Base] user, updating its token
+    #
+    # @raise [RefreshError] for invalid credentials
+    # @raise [InvalidResponseError] for generic server error
+    def login!
       params = { key: 'r4g4zz3tt1', matricola: @student_id,
                  stringaAutenticazione: @password }
-      headers = { 'Content-EventType' => 'application/x-www-form-urlencoded',
-                  Accept: 'application/json' }
-      opt = { body: params, headers: headers }
-      res = JSON.parse HTTParty.post("#{ENDPOINT_API}/autenticazione", opt).body
-      @token = res['output']
-      handle_login_errors res
+      res = Phoenix.new(body: params).login
+      validate_login! res
     end
 
     private
 
-    def handle_login_errors(res)
-      x = res['esito']['flagEsito']
+    # @param [Hash] response: the parsed response
+    #
+    # @raise [RefreshError] for invalid credentials
+    # @raise [InvalidResponseError] for generic server error
+    def validate_login!(response)
+      @token = response['output']
+      x = response['esito']['flagEsito']
       return if x.zero?
       raise RefreshError, 'Invalid credentials when refreshing token' if x == -4
       raise RefreshError.new('Password expired').expired_password! if x == -2
