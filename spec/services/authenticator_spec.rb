92167436eb562030f5ef04cd9a9b371b32a14d84
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-25 09:32:57 +0200

    improve spec

--- spec/services/authenticator_spec.rb
+++ spec/services/authenticator_spec.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
-RSpec.describe Openstud do
-  it 'successfully logins and get infos' do
-    base = Openstud::Base.new
-    base.student_id = ENV['STUDENT_ID']
-    base.password = ENV['STUDENT_PWD']
-    base.login!
-    expect(base.token.empty?).to be false
-    base.info_student!
+RSpec.describe 'Authenticator' do
+  context 'for valid credentials' do
+    it 'get infos' do
+      base = Openstud::Base.new
+      base.student_id = ENV['STUDENT_ID']
+      base.password = ENV['STUDENT_PWD']
+      expect(base.info_student!).to eq 'LUIGI'
+    end
   end
 
-  it 'raises RefreshException for invalid credentials' do
-    base = Openstud::Base.new student_id: '1234567', password: 'password'
-    expect { base.login! }.to raise_error Openstud::Exceptions::RefreshError
-  end
+  context 'when credentials are invalid' do
+    it 'fails to login' do
+      base = Openstud::Base.new student_id: '1234567', password: 'password'
+      expect { base.login! }.to raise_error Openstud::Exceptions::RefreshError
+    end
 
-  it 'raises InvalidCredentials' do
-    base = Openstud::Base.new student_id: '1234567', password: 'password'
-    expect { base.info_student! }
-      .to raise_error Openstud::Exceptions::InvalidCredentialsError
+    it 'fails to geti infos' do
+      base = Openstud::Base.new student_id: '1234567', password: 'password'
+      expect { base.info_student! }
+        .to raise_error Openstud::Exceptions::InvalidCredentialsError
+    end
   end
 end
