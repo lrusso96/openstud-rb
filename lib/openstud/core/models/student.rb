a32b75ce85b42ae44c9506305e289793dffe1a15
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-26 12:07:22 +0200

    student stub

--- lib/openstud/core/models/student.rb
+++ lib/openstud/core/models/student.rb
@@ -2,5 +2,28 @@
 
 module Openstud
   class Student
+    attr_accessor :fiscal_code
+    attr_accessor :first_name
+    attr_accessor :last_name
+    attr_accessor :birth_date
+    attr_accessor :birth_city
+    attr_accessor :birth_place
+    attr_accessor :course_year
+    attr_accessor :first_enrollment
+    attr_accessor :last_enrollment
+    attr_accessor :department_name
+    attr_accessor :course_name
+    attr_accessor :nation
+    attr_accessor :email
+    attr_accessor :citizenship
+    attr_accessor :gender
+    attr_accessor :student_status
+    attr_accessor :academic_year
+    attr_accessor :academic_year_course
+    attr_accessor :student_id
+    attr_accessor :code_course
+    attr_accessor :type_student
+    attr_accessor :cfu
+    attr_accessor :erasmus
   end
 end
