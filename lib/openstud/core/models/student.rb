192c3b27698f1caa13c8dd74ea203a1e454e8d11
luigi <russo.1699981@studenti.uniroma1.it>
2019-07-26 12:23:49 +0200

    cleanup

--- lib/openstud/core/models/student.rb
+++ lib/openstud/core/models/student.rb
@@ -1,29 +1,13 @@
 # frozen_string_literal: true
 
 module Openstud
+  # Model for student
   class Student
-    attr_accessor :fiscal_code
-    attr_accessor :first_name
-    attr_accessor :last_name
-    attr_accessor :birth_date
-    attr_accessor :birth_city
-    attr_accessor :birth_place
-    attr_accessor :course_year
-    attr_accessor :first_enrollment
-    attr_accessor :last_enrollment
-    attr_accessor :department_name
-    attr_accessor :course_name
-    attr_accessor :nation
-    attr_accessor :email
-    attr_accessor :citizenship
-    attr_accessor :gender
-    attr_accessor :student_status
-    attr_accessor :academic_year
-    attr_accessor :academic_year_course
-    attr_accessor :student_id
-    attr_accessor :code_course
-    attr_accessor :type_student
-    attr_accessor :cfu
-    attr_accessor :erasmus
+    attr_accessor :fiscal_code, :first_name, :last_name, :gender, :email,
+                  :birth_date, :birth_city, :birth_place, :nation, :citizenship
+    attr_accessor :student_id, :first_enrollment, :last_enrollment,
+                  :course_year, :student_status, :academic_year, :cfu,
+                  :academic_year_course, :code_course, :type_student, :erasmus,
+                  :department_name, :course_name
   end
 end
--- lib/openstud/error.rb
+++ lib/openstud/error.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# @abstract
 class Class
   alias err_type bool_accessor
 end
