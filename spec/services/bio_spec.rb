354e195e82da71f0943ea7dfc9c27dfe4a93ae6f
luigi <russo.1699981@studenti.uniroma1.it>
2019-08-04 09:46:51 +0200

    new spec for bio

--- spec/services/bio_spec.rb
+++ spec/services/bio_spec.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Authenticator' do
+RSpec.describe 'Bio' do
   context 'for valid credentials' do
-    it 'logins' do
+    it 'get infos' do
       base = Openstud::Base.new
       base.student_id = ENV['STUDENT_ID']
       base.password = ENV['STUDENT_PWD']
-      base.login!
-      expect(base.token.empty?).to be false
+      st = base.info_student!
+      expect(st.student_id).to eq base.student_id
+      expect(st.citizenship.empty?).to be false
     end
   end
 
   context 'when credentials are invalid' do
-    it 'fails to login' do
+    it 'fails to get infos' do
       base = Openstud::Base.new student_id: '1234567', password: 'password'
-      expect { base.login! }
-        .to raise_error Openstud::Exceptions::UserNotEnabledError
+      expect { base.info_student! }
+        .to raise_error Openstud::Exceptions::CredentialsError
     end
   end
 end
